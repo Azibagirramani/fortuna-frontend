15d8e93228fb62122a87e756161992cb0eb02e1f
Kelvin Mansi <kelvinmansi2@gmail.com>
2022-05-22 14:56:56 -0400

    WIP: ui fix

--- components/public/BaseFooter.tsx
+++ components/public/BaseFooter.tsx
@@ -7,25 +7,25 @@ export default () => {
     const footerLinks: FooterLinks[] = [
         {
             title: "Pages",
-            links: [ 
-                
+            links: [
+
                 {
                     label: "Teams",
-                    to: ""
-                  },
-                  {
+                    to: "#"
+                },
+                {
                     label: "Contacts",
                     to: ""
-                  },
-                  {
+                },
+                {
                     label: "Issues",
-                    to:""
-                  },
-                  {
+                    to: ""
+                },
+                {
                     label: "Beta - login",
-                    to:""
-                  }
-             ]
+                    to: ""
+                }
+            ]
         },
         {
             title: "Product Features",
@@ -45,37 +45,37 @@ export default () => {
                 }
             ]
         }
-    ] 
+    ]
 
-    return(
-            <footer className="flex flex-col gap-9 xl:flex-row md:flex-row lg:justify-between mt-10 mx-5 lg:mx-[4rem] xl:mx-[10rem] my-10">
-                <div className="flex flex-col lg:justify-between">
-                    <h1 className="text-2xl font-extrabold">Fortuna</h1> 
-                    <div>
-                        <p className="mt-5">@2021 Lorem ipsum dolor sit </p>
-                        <p>amet consectetur adipisicing elit.</p>
-                    </div>
+    return (
+        <footer className="flex flex-col gap-9 xl:flex-row md:flex-row lg:justify-between mx-5 lg:mx-[4rem] xl:mx-[10rem] xl:p-0 lg:p-0 pt-10">
+            <div className="flex flex-col lg:justify-between">
+                <h1 className="text-2xl font-extrabold">Fortuna</h1>
+                <div>
+                    <p className="mt-5">@2021 Lorem ipsum dolor sit </p>
+                    <p>amet consectetur adipisicing elit.</p>
                 </div>
-                { footerLinks.map((items, index)=> {
-                    return <div className="" key={index}>
-                        <h3  className="font-extrabold">{items.title}</h3>
-                        <ul className="mt-4">
-                            { items.links?.map((links, linkIndex)=> {
-                                return<li className="mb-3 last:mb-0" key={linkIndex}>
-                                    <Link href={links?.to}>
-                                        <a className="font-normal text-black hover:text-moony">{links.label}</a>
-                                    </Link>
-                                </li>
-                            }) }
-                        </ul>
-                    </div>
-                }) }
+            </div>
+            {footerLinks.map((items, index) => {
+                return <div className="" key={index}>
+                    <h3 className="font-extrabold">{items.title}</h3>
+                    <ul className="mt-4">
+                        {items.links?.map((links, linkIndex) => {
+                            return <li className="mb-3 last:mb-0" key={linkIndex}>
+                                <Link href={links?.to}>
+                                    <a className="font-normal text-black hover:text-moony">{links.label}</a>
+                                </Link>
+                            </li>
+                        })}
+                    </ul>
+                </div>
+            })}
 
-                <div className="">
-                    <h1 className="text-2xl font-extrabold">Get in touch</h1> 
-                    <p className="mt-2">Questions or feedbacks? </p>
-                    <p className="">We'd love to hear from you </p>
-                </div>
-            </footer>
+            <div className="">
+                <h1 className="text-2xl font-extrabold">Get in touch</h1>
+                <p className="mt-2">Questions or feedbacks? </p>
+                <p className="">We'd love to hear from you </p>
+            </div>
+        </footer>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,18 +9,18 @@ import HeroImage from "../public/images/Bikes.svg"
 import BaseButton from "../components/forms/BaseButton";
 
 
-function Home (){
+function Home() {
   return (
-    <>  
-    <Head>
-      <title>
-        Welcome to Fortuna 
-      </title>
-    </Head>
+    <>
+      <Head>
+        <title>
+          Welcome to Fortuna
+        </title>
+      </Head>
       <div className="flex justify-center lg:flex-row md:flex-row flex-col mt-10 gap-10 mx-5 lg:mx-[4rem] xl:mx-[10rem]">
         <div className="flex-1 text-center mx-auto lg:text-left md:text-left">
-          <h1 className="text-[1.8rem] md:text-[2rem] lg:text-[2.5rem] xl:text-7xl xl:leading-normal text-black-100 font-extrabold leading-normal">Share your smile with <br/> the world and <br className="d-none" /> find a friend</h1>
-          <p className="mt-[2rem] text-black-100 xl:text-2xl xl:font-medium lg:font-medium font-extrabold">Thousands of new profiles delivered <br/> weekly to your inbox</p>
+          <h1 className="text-[1.8rem] md:text-[2rem] lg:text-[2.5rem] xl:text-7xl xl:leading-normal text-black-100 font-extrabold leading-normal">A small donation can make a huge difference</h1>
+          <p className="mt-[2rem] text-black-100 xl:text-2xl xl:font-medium lg:font-medium font-extrabold">Do a good deed by giving to those in need <br /> Don’t delay. Give today!</p>
           <div className="flex flex-col mt-5">
             <BaseButton type="button">
               <span className="font-capitalize font-extrabold text-[white]">Join Beta Team</span>
@@ -28,13 +28,13 @@ function Home (){
           </div>
         </div>
         <div className="md:flex-1 -z-10">
-          <Image 
+          <Image
             src={HeroImage}
           />
         </div>
       </div>
     </>
- 
+
   )
 }
 
