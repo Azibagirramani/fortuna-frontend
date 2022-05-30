0aa4edc339c98efc42b019bd84237e11a540df75
Kelvin Mansi <kelvinmansi2@gmail.com>
2022-05-30 07:17:53 -0400

    WIP: ui fix

--- components/public/BaseFooter.tsx
+++ components/public/BaseFooter.tsx
@@ -1,8 +1,8 @@
 export default () => {
 
     return (
-        <footer className="flex justify-center">
-            <div className="flex gap-4 absolute bottom-0 text-base md:text-lg pb-8 text-gray-400 hover:text-gray-400">
+        <footer className="flex justify-center mt-5 md:mt-0 lg:mt-0 xl:mt-5">
+            <div className="flex gap-4 md:absolute md:bottom-0 text-base md:text-lg pb-8 text-gray-400 hover:text-gray-400">
                 <a href="#" className="hover:text-subSecondary text-gray-light font-medium xl:text-lg">Fortuna @2022</a>
                 <ul className="flex gap-6">
                     <li>
